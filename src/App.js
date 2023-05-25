6609b02842119ac8a4cd4b7a0a0f376cfa8f4437
Sungho <hurgj123kr@gmail.com>
2023-05-25 21:19:46 +0900

    react useEffect use

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import style from "./App.module.css"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 function App() {
   const [counter, setCounter] = useState(0)
   const counterUp = () => setCounter((current) => current + 1)
+  useEffect(() => {
+    console.log("이건 한번만 실행이 됩니다.")
+  }, [])
   return (
     <div>
       <h2>{counter}</h2>
