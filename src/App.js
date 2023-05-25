39aac4491182f6cfb2ad06e4705a4046eee7a640
Sungho <hurgj123kr@gmail.com>
2023-05-25 21:01:51 +0900

    react effect use already

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Button from "./Button.js"
 import style from "./App.module.css"
-
+import { useState } from "react"
 function App() {
+  const [counter, setCounter] = useState(0)
+  const counterUp = () => setCounter((current) => current + 1)
   return (
     <div>
-      <h2 className={style.title}>Welcome React</h2>
-      <Button text={"Home"} />
+      <h2>{counter}</h2>
+      <button className={style.btn} onClick={counterUp}>
+        Click
+      </button>
     </div>
   )
 }
