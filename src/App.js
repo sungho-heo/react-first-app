da15534cb139ee916df80130e9e81aece4e066da
Sungho <hurgj123kr@gmail.com>
2023-05-26 17:01:11 +0900

    todo react already

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import style from "./App.module.css"
-import { useState, useEffect } from "react"
+import { useState } from "react"
+
 function App() {
-  const [counter, setCounter] = useState(0)
-  const counterUp = () => setCounter((current) => current + 1)
-  const [word, changeWord] = useState("")
-  const onChange = (event) => changeWord(event.target.value)
-  const Hello = () => {
-    useEffect(() => {
-      console.log("I'm here")
-      return () => console.log("bye")
-    }, [])
-    return <h1>Hello</h1>
+  const [todo, changeTodo] = useState("")
+  const [todoList, setTodos] = useState([])
+  const onChange = (event) => changeTodo(event.target.value)
+  const onSubmit = (event) => {
+    event.preventDefault()
+    if (todo === "") {
+      return
+    }
+    setTodos((current) => [todo, ...todoList])
+    changeTodo("")
   }
-  const [showing, setShow] = useState(false)
-  const onClick = () => setShow((current) => !current)
   return (
     <div>
-      <input value={word} onChange={onChange} placeholder='Search'></input>
-      <h2>{counter}</h2>
-      <button className={style.btn} onClick={counterUp}>
-        Click
-      </button>
-      <hr />
-      <div>
-        {showing ? <Hello /> : null}
-        <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      </div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type='text'
+          placeholder='Todo'
+        ></input>
+        <button>Add To do</button>
+      </form>
     </div>
   )
 }
