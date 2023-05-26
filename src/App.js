84eb8fb86be2d2638ef3964ad741ce965dfe9026
Sungho <hurgj123kr@gmail.com>
2023-05-26 15:05:24 +0900

    react useEffect specific conditions [] use

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import { useState, useEffect } from "react"
 function App() {
   const [counter, setCounter] = useState(0)
   const counterUp = () => setCounter((current) => current + 1)
+  const [word, changeWord] = useState("")
+  const onChange = (event) => changeWord(event.target.value)
   useEffect(() => {
     console.log("이건 한번만 실행이 됩니다.")
   }, [])
+  useEffect(() => {
+    console.log("Change word", word)
+  }, [word])
+  useEffect(() => {
+    console.log("Change count", counter)
+  }, [counter])
   return (
     <div>
+      <input value={word} onChange={onChange} placeholder='Search'></input>
       <h2>{counter}</h2>
       <button className={style.btn} onClick={counterUp}>
         Click
