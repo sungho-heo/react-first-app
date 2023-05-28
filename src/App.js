574aeb7d05e6a4a4b54f27d11991dcb439bf36e6
Sungho <hurgj123kr@gmail.com>
2023-05-28 19:49:42 +0900

    react-router-dom use and Route first..

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./routes/Home.js"
+import Detail from "./routes/Detail.js"
 function App() {
-  return null
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/movies' element={<Detail />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types"
+import { Link } from "react-router-dom"
 function Movie({ coverImg, title, year, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>Title:{title}</h2>
+      <h2>
+        <Link to='/movies'> Title:{title}</Link>
+      </h2>
       <span>Year:{year}</span>
       <p>Description:{summary}</p>
       <span>Genres</span>
